d4d56af25ddcc688ee6e5345293a949820cdc13b
Mr Robot <akhilp1708@gmail.com>
2019-02-18 18:51:06 +0530

    Updated few functions

--- reset_cpanel.sh
+++ reset_cpanel.sh
@@ -48,9 +48,9 @@ cyn=$'\e[1;36m'
 white=$'\e[0m'
 ylw=$'\e[1;33'
 
-RESTORE=`sudo /scripts/restorepkg`
-PACKAGE=`sudo /scripts/pkgacct`
-WHOOWNS=`sudo /scripts/whoowns`
+RESTORE="sudo /scripts/restorepkg"
+PACKAGE="sudo /scripts/pkgacct"
+WHOOWNS="sudo /scripts/whoowns"
 FILE=$(echo $BACKUP | cut -d'/' -f3)
 
 #Auto accept rsa key fingerprint from command line
@@ -71,64 +71,62 @@ FILE=$(echo $BACKUP | cut -d'/' -f3)
          "$@"
  }
 # use sshtmp, or scptmp in place of ssh and scp
-echo ""
-read -p "Enter your WSS username : " WSS
-read -p "Enter the server IP : " SERVER
-read -p "Enter the cPanel username : " USER
+
+read -p "$blu Enter your WSS username $white: " WSS
+read -p "$blu Enter the server IP $white: " SERVER
+read -p "$blu Enter the cPanel username $white: " USER
 echo ""
 
 sshtmp -l $WSS $SERVER /bin/bash << EOF
-
-echo "Taking a backup of the account $USER for saftey ....."
-sleep 1s
+echo ""
+sleep 3s
+echo "$grn Taking a Full backup of the account $USER for saftey .....$white"
+sleep 5s
 echo ""
 sudo /scripts/pkgacct $USER
 echo ""
-echo "BACKUP COMPLETED"
-echo "" ; echo "Backup stored in $SERVER /home/" ; echo ""
-echo -e "Now creating a backup archive without $USER home files, MySQL data etc...."
+sleep 5s
+echo "$grn FULL BACKUP COMPLETED $white" ; echo "" ; echo ""
+sleep 5s
+echo "$mag Moving the Full backup to /home/$WSS ..... $white"
+sudo mv /home/cpmove-$USER.tar.gz /home/$WSS/
+sleep 6s
+echo "" ; echo "$grn The Full Backup stored in server $SERVER in location /home/$WSS/cpmove-$USER.tar.gz  $white" ; echo ""
+sleep 6s
+echo -e "$cyn Now creating a backup archive without $USER home files, MySQL data etc....  $white"
 echo ""
-sleep 1s
+sleep 5s
 sudo /usr/local/cpanel/scripts/pkgacct --skipacctdb --skipdnszones --skipdomains --skipftpusers --skiphomedir --skipintegrationlinks --skiplogs --skipmailconfig --skipmailman --skipmysql --skippgsql --skipssl --skipuserdata --skipshell $USER
 echo ""
-echo "ARCHIVE CREATED"
-echo ""
-echo "Enter the Backup archive file name including the path : " BACKUP
-echo ""
-echo "BACKUP PATH STORED" ; echo ""
-echo "Now terminating the account $USER from the system......."
-echo ""
-echo "Note: Please note that all the user's data will be deleted. So, backup the user's data (like data under public_html), if required."
-echo ""
-read -p "Continue with removing the account? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-echo ""
+sleep 4s
+echo "$grn Archive Created without the user home files $white"
+EOF
+
+
+echo "" ; sleep 5s 
+read -p "$blu Enter the pkgacctfile $white: " BACKUP
+echo "" ; sleep 5s ; echo ""
+echo "$cyn BACKUP PATH STORED, THANK FOR YOUR INPUT...!! $white" ; echo "" ; echo "" ; sleep 5s
+echo "$red Now terminating the account $USER from the system....... $white"
+echo "" ; echo "" ; sleep 5s
+echo "$red Note: Please note that all the user's data will be deleted..!! $white"
+echo "" ; echo "" ; sleep 5s
+
+
+read -p "$red Continue with removing the account? $white (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo "" ; echo "" ; sleep 5s
+
+sshtmp -l $WSS $SERVER /bin/bash << EOF
 sudo /usr/local/cpanel/scripts/removeacct  --force $USER
-echo ""
-echo -e "ACCOUNT REMOVAL COMPLETE"
-echo ""
-echo "Restoring the $USER cPanel account in $SERVER in DEFAULT STATE"
-echo""
-sudo /usr/local/cpanel/scripts/restorepkg /home/$FILE
-echo ""
-echo "Package Restored........RESET COMPLETE, ENJOY..!"
+echo "" ; echo "" ; sleep 5s
+echo -e "$grn ACCOUNT REMOVAL COMPLETE $white"
+echo "" ;echo "" ; sleep 5s
+echo "$mag Restoring the $USER cPanel account in $SERVER in DEFAULT STATE $white"
+echo"" ; echo "" ; sleep 4s 
+sudo /usr/local/cpanel/scripts/restorepkg $BACKUP
+echo "" ; echo "" ; sleep 5s
+echo "$grn Package Restored........RESET COMPLETE, ENJOY.. $white!" ; echo ""
 EOF
 
 #Logging the use of the script.
 echo "[`date`] [`whoami`] Executed the Migration_script" >> /home/akhil.pra/execution.log
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
