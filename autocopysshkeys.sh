37ffe6e7b3defe8f0ff2ddabdac40d91a0fc5fa8
Mr Robot <akhilp1708@gmail.com>
2019-02-20 13:13:55 +0530

    version 1.0
    It automates the process of uploading your SSH key to a remote host that you SSH into. 
        Creates a .ssh directory if there is not one already there, and sets the correct permissions on it.
        Puts your key in the authorized_keys file (and creates it if it was not there already), and changes the permissions on it.

---
+++ autocopysshkeys.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#Creates a .ssh directory if there is not one already there, and sets the correct permissions on it.
+#Puts your key in the authorized_keys file (and creates it if it was not there already), and changes the permissions on it.
+
+KEY="$HOME/.ssh/id_dsa.pub"
+
+if [ ! -f ~/.ssh/id_dsa.pub ];then
+    echo "private key not found at $KEY"
+    echo "* please create it with "ssh-keygen -t dsa" *"
+    echo "* to login to the remote host without a password, don't give the key you create with ssh-keygen a password! *"
+    exit
+fi
+
+if [ -z $1 ];then
+    echo "Please specify user@host.tld as the first switch to this script"
+    exit
+fi
+
+echo "Putting your key on $1... "
+
+KEYCODE=`cat $KEY`
+ssh -q $1 "mkdir ~/.ssh 2>/dev/null; chmod 700 ~/.ssh; echo "$KEYCODE" >> ~/.ssh/authorized_keys; chmod 644 ~/.ssh/authorized_keys"
+
+echo "done!"
