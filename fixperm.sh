51a518da211556f776687c062d80d81cf885e9ff
Mr Robot <akhilp1708@gmail.com>
2019-02-24 17:46:59 +0530

    Updating cpanel user permission script
    The fixperms script is intended for cPanel servers only. It is dependent on cPanel’s internal scripts and file structure.

--- fixperm.sh
+++ fixperm.sh
@@ -11,6 +11,27 @@ fi
 
 USER=$@
 
+  #Check account against cPanel users file
+  if ! grep $account /var/cpanel/users/*
+  then
+    tput bold
+    tput setaf 1
+    echo "Invalid cPanel account"
+    tput sgr0
+    exit 0
+  fi
+
+  #Make sure account isn't blank
+  if [ -z $account ]
+  then
+    tput bold
+    tput setaf 1
+    echo "Need an account name!"
+    tput sgr0
+    helptext
+  #Else, start doing work
+  else
+
 for user in $USER
 do
 
